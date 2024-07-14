53d59554223153b9af6f79bdd58e2bafad294cdd
Muhammad Moiz <moiz67932@gmail.com>
2024-07-14 14:43:55 +0500

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '02c29c56edmshb0ed94f6f852aedp177af6jsnb98e9625b260',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '02c29c56edmshb0ed94f6f852aedp177af6jsnb98e9625b260',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
